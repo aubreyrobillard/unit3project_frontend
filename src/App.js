de0878f093c9d682c8404e9290f8b84c8f22ce12
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-08-19 16:38:41 -0500

    App.js outlet and header included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import StarRating from "./Components/StarRating";
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Outlet/>
       <StarRating />
     </div>
   );
