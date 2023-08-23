e95aafbec5b632d7907a9ecb79214a1d511ddd99
Taylor Murdock <tjmurd4545@gmail.com>
2023-08-23 12:22:29 -0400

    some button redirect work and background img for all screen sizes for new recipe page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Header(props) {
-    return <nav className="Nav">
-        <Link to='/'>
-            <div>Taste Tally App</div>
-        </Link>
+  return (
+    <nav className="Nav">
+      <Link to="/dashboard">
+        <div>Taste Tally App</div>
+      </Link>
     </nav>
+  );
 }
 
-export default Header
+export default Header;
