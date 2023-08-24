7048dc5cc5d0ab2f19cd6ba45b29f3689de475ec
Taylor Murdock <tjmurd4545@gmail.com>
2023-08-23 22:47:16 -0400

    css for login/signup is done

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,6 +1,41 @@
+import React, { useState } from "react";
+
 function Homepage(props) {
-    return <h1>Welcome to Taste Tally!</h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const login = () => {
+    // (no actual authentication here).
+    setLoggedIn(true);
+  };
+
+  const signUp = () => {
+    // (no actual registration here).
+    setLoggedIn(true);
+  };
+
+  return (
+    <div className="homepage-container">
+      <div className="login-form">
+        <h2>Welcome to Taste Tally!</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={login}>Login</button>
+        <button onClick={signUp}>Sign Up</button>
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
-
+export default Homepage;
